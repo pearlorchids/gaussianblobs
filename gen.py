425c622dcd74a4d50f7718bc7f56ee28d5e1131b
pearlorchids <sharma.sweta1990@gmail.com>
2021-01-11 16:14:38 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -27,7 +27,7 @@ pyplot.show()
 f = open("coords.txt", "a")
 
 #f.write(str(n)+","+str((min(X[:,0])+max(X[:,0]))/2)+","+str((min(X[:,1])+max(X[:,1]))/2))
-n=6085
+n=1
 
         
 for i in arange(0,0.015,0.00025):
@@ -40,8 +40,8 @@ for i in arange(0,0.015,0.00025):
         #ax.set_position([i,j,2,2], which='both')
         #print(len(x_x))
         #pyplot.axis('off')
-        print(i,max(x_x))
-        print(j,max(y_y))
+        #print(i,max(x_x))
+        #print(j,max(y_y))
         ax.set_facecolor('black')
         pyplot.xlim(0,0.02)
         pyplot.ylim(0,0.02)
@@ -86,8 +86,8 @@ for i in arange(0,0.015, 0.00025):
         #pyplot.autoscale(enable=False, axis='both', tight=None)
         #ax.get_xaxis().set_ticks([])
         #ax.get_yaxis().set_ticks([])
-        print(i,min(x_x))
-        print(j,min(y_y))
+        #print(i,min(x_x))
+        #print(j,min(y_y))
         #fig.savefig('1.png',bbox_inches='tight', pad_inches = 0)
         if(((min(x_x)<0.019))&((min(y_y)<0.019))):
             fig.savefig(str(n)+'.png',bbox_inches='tight', pad_inches = 0)
@@ -121,8 +121,8 @@ for i in arange(0,0.015, 0.00025):
         #pyplot.autoscale(enable=False, axis='both', tight=None)
         #ax.get_xaxis().set_ticks([])
         #ax.get_yaxis().set_ticks([])
-        print(i,min(x_x))
-        print(j,max(y_y))
+        #print(i,min(x_x))
+        #print(j,max(y_y))
         #fig.savefig('1.png',bbox_inches='tight', pad_inches = 0)
         if(((min(x_x)<0.019))&((max(y_y)>0.0009))):
             fig.savefig(str(n)+'.png',bbox_inches='tight', pad_inches = 0)
@@ -154,8 +154,8 @@ for i in arange(0,0.015,0.00025):
         #pyplot.autoscale(enable=False, axis='both', tight=None)
         #ax.get_xaxis().set_ticks([])
         #ax.get_yaxis().set_ticks([])
-        print(i,max(x_x))
-        print(j,min(y_y))
+        #print(i,max(x_x))
+        #print(j,min(y_y))
         #fig.savefig('1.png',bbox_inches='tight', pad_inches = 0)
         if(((max(x_x)>0.0009))&((max(y_y)>0.0009))):
             fig.savefig(str(n)+'.png',bbox_inches='tight', pad_inches = 0)
